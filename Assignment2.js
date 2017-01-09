40cbe148ccd058e825a5c1a6fc4fbcbfe6d094fe
Naveenreddivari <naveenreddyvari@gmail.com>
2017-01-09 19:40:30 +0530

    Session6-Assignment2
    Assignment2

---
+++ Assignment2.js
@@ -0,0 +1,15 @@
+var a= 110;
+var b= 100;
+var c= 70;
+if(a>b && a>c)
+{
+console.log('Greatest number is A' + ' Value = ' +a);
+}
+else if(b>a && b>c)
+{
+console.log('Greatest number is B' + ' Value = ' +b);
+}
+else
+{
+console.log('Greatest number is C' + ' Value = ' +c);
+}
